8bcf5c5cd466fdd3f0ebb9eca303998bbfba880a
Артем <amstelart@MacBook-Pro-Artem.local>
2017-12-24 22:02:09 +0300

    add main

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,49 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+
+  $("#main-slider").owlCarousel({
+    items: 1,
+    nav: true,
+    dots: false,
+    animateOut: 'fadeOut',
+    autoplay: true,
+    loop: true,
+    // navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 3px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 3px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    center: true
+  });
+
+  $("#specialists-carousel").owlCarousel({
+    items: 1,
+    nav: true,
+    dots: false,
+    animateOut: 'fadeOut',
+    autoplay: true,
+    loop: true,
+    // navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 3px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 3px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    center: true
+  });
+
+  $("#license-carousel").owlCarousel({
+    items: 3,
+    margin: 30,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    loop: true
+    // navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 3px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 3px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+  });
+
+  $(".gallery-carousel").owlCarousel({
+    items: 3,
+    margin: 30,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    loop: true
+    // navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 3px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 3px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+  });
+
+});
 
 // Изоляция без jQuery
 // (function(){
