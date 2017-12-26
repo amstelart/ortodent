82edc192fc3c73c591f0b0b3e02d7fadf135bd07
Артем <amstelart@MacBook-Pro-Artem.local>
2017-12-26 07:45:59 +0300

    Update 2017-12-26T04:45:57.092Z

--- js/global-script.js
+++ js/global-script.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
     nav: true,
     dots: true,
     animateOut: 'fadeOut',
-    autoplay: true,
+    // autoplay: true,
     loop: true,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     center: true
@@ -30,7 +30,30 @@ $( document ).ready(function() {
     dots: false,
     autoplay: true,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-    loop: true
+    loop: true,
+    responsive : {
+      0 : {
+        items: 1,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 1,
+      },
+      768 : {
+        items: 2,
+      },
+      992 : {
+        items: 3,
+      },
+      1200 : {
+        items: 3,
+      },
+      1800 : {
+        items: 3,
+      }
+    }
   });
 
   $(".gallery-carousel").owlCarousel({
@@ -40,7 +63,30 @@ $( document ).ready(function() {
     dots: false,
     autoplay: true,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-    loop: true
+    loop: true,
+    responsive : {
+      0 : {
+        items: 1,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 1,
+      },
+      768 : {
+        items: 2,
+      },
+      992 : {
+        items: 3,
+      },
+      1200 : {
+        items: 3,
+      },
+      1800 : {
+        items: 3,
+      }
+    }
   });
 
 });
