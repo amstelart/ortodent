686946c0781ad8b0383f5f1c4441f6f8bb1c47d3
Артем <amstelart@MacBook-Pro-Artem.local>
2017-12-26 14:15:59 +0300

    fix

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -61,7 +61,7 @@ $( document ).ready(function() {
     margin: 30,
     nav: true,
     dots: false,
-    autoplay: true,
+    // autoplay: true,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     loop: true,
     responsive : {
