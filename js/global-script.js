6871f199e76600d65b0819ac7158f4e32670a422
Артем <amstelart@MacBook-Pro-Artem.local>
2018-01-30 14:27:42 +0300

    Update 2018-01-30T11:27:40.584Z

--- js/global-script.js
+++ js/global-script.js
@@ -6,7 +6,8 @@ $( document ).ready(function() {
     nav: true,
     dots: true,
     animateOut: 'fadeOut',
-    // autoplay: true,
+    autoplay: true,
+    smartSpeed: 500,
     loop: true,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     center: true
